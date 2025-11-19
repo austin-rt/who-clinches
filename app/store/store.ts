7f4c545b2d1769d58d65b73d16d855e822b21423
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2025-11-19 09:54:33 -0500

    refactor store to use redux-persist for state persistence

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,16 +1,49 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import uiReducer from './uiSlice';
 import gamePicksReducer from './gamePicksSlice';
 import { apiSlice } from './apiSlice';
 
-export const store = configureStore({
-  reducer: {
-    ui: uiReducer,
-    gamePicks: gamePicksReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
-  },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+// Persist config for ui slice - exclude lastUpdated (server data, not user preference)
+const uiPersistConfig = {
+  key: 'ui',
+  storage,
+  blacklist: ['lastUpdated'],
+};
+
+// Persist config for gamePicks slice
+const gamePicksPersistConfig = {
+  key: 'gamePicks',
+  storage,
+};
+
+// Create persisted reducers
+const persistedUiReducer = persistReducer(uiPersistConfig, uiReducer);
+const persistedGamePicksReducer = persistReducer(gamePicksPersistConfig, gamePicksReducer);
+
+// Combine reducers (API slice is not persisted)
+const rootReducer = combineReducers({
+  ui: persistedUiReducer,
+  gamePicks: persistedGamePicksReducer,
+  [apiSlice.reducerPath]: apiSlice.reducer,
 });
+
+// Configure store with persisted reducer
+// Thunk is included by default in getDefaultMiddleware
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore redux-persist actions
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE', 'persist/PURGE'],
+      },
+    }).concat(apiSlice.middleware),
+});
+
+// Create persistor for PersistGate
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
