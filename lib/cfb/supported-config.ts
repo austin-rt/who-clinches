950a98c4a2f30b3293e5ceca79ab08427790e969
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2025-11-25 16:05:54 -0500

    remove obsolete files and routes

--- lib/cfb/supported-config.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Supported sports and conferences configuration
- * Maintain this list as new sports/conferences are added
- */
-
-export const SUPPORTED_SPORTS_CONFS = [{ sport: 'cfb', conf: 'sec' }] as const;
