4cbd450a7e85ff2cb3af31754801f932bb166ed2
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2025-12-09 10:15:40 -0500

    rename ConferenceTiebreakerConfig to CFBConferenceTiebreakerConfig

--- lib/cfb/tiebreaker-rules/core/types.ts
+++ lib/cfb/tiebreaker-rules/core/types.ts
@@ -1,16 +1,11 @@
 import { GameLean } from '../../../types';
 import { RuleResult } from '../common/types';
 
-export interface TiebreakerRuleConfig {
+export interface CFBTiebreakerRuleConfig {
   name: string;
-  apply: (
-    tiedTeams: string[],
-    games: GameLean[],
-    allTeams?: string[]
-  ) => RuleResult;
+  apply: (tiedTeams: string[], games: GameLean[], allTeams?: string[]) => RuleResult;
 }
 
-export interface ConferenceTiebreakerConfig {
-  rules: TiebreakerRuleConfig[];
+export interface CFBConferenceTiebreakerConfig {
+  rules: CFBTiebreakerRuleConfig[];
 }
-
