8c73035ec4fbcc697388fec8f86cddfd545cea2d
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2025-12-09 10:15:43 -0500

    update SEC and MAC configs to use CFBConferenceTiebreakerConfig

--- lib/cfb/tiebreaker-rules/mac/config.ts
+++ lib/cfb/tiebreaker-rules/mac/config.ts
@@ -1,11 +1,11 @@
-import { ConferenceTiebreakerConfig } from '../core/types';
+import { CFBConferenceTiebreakerConfig } from '../core/types';
 import { applyRuleAHeadToHead } from '../common/rule-a-head-to-head';
 import { applyRuleBCommonOpponents } from '../common/rule-b-common-opponents';
 import { applyRuleETeamRatingScore } from '../common/rule-e-team-rating-score';
 import { applyRuleCHighestPlacedOpponent } from '../common/rule-c-highest-placed-opponent';
 import { applyRuleDOpponentWinPercentage } from '../common/rule-d-opponent-win-percentage';
 
-export const MAC_TIEBREAKER_CONFIG: ConferenceTiebreakerConfig = {
+export const CFB_MAC_TIEBREAKER_CONFIG: CFBConferenceTiebreakerConfig = {
   rules: [
     {
       name: 'Head-to-Head',
@@ -29,4 +29,3 @@ export const MAC_TIEBREAKER_CONFIG: ConferenceTiebreakerConfig = {
     },
   ],
 };
-
