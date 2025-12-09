faf26592f5d51c40837a37ac022c5aeb283864d7
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2025-12-09 10:16:20 -0500

    remove old configs file after moving to constants

--- lib/cfb/tiebreaker-rules/configs.ts
+++
@@ -1,8 +0,0 @@
-import { ConferenceTiebreakerConfig } from './core/types';
-import { SEC_TIEBREAKER_CONFIG } from './sec/config';
-import { MAC_TIEBREAKER_CONFIG } from './mac/config';
-
-export const CONFERENCE_CONFIGS: Record<string, ConferenceTiebreakerConfig> = {
-  SEC: SEC_TIEBREAKER_CONFIG,
-  MAC: MAC_TIEBREAKER_CONFIG,
-};
