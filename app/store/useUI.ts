01e5667ecc76bb361b9597105a68c08561e2f40a
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2025-12-09 09:07:42 -0500

    memoize useUI hook functions with useCallback

--- app/store/useUI.ts
+++ app/store/useUI.ts
@@ -1,26 +1,39 @@
 import { useAppDispatch, useAppSelector } from './hooks';
 import { setTheme, setMode, setView, setHideCompletedGames } from './uiSlice';
 import { ThemeMode, ViewMode } from '@/types/frontend';
+import { useCallback } from 'react';
 
 export const useUIState = () => {
   const dispatch = useAppDispatch();
   const { theme, mode, view, hideCompletedGames } = useAppSelector((state) => state.ui);
 
-  const updateTheme = (newTheme: string) => {
-    dispatch(setTheme(newTheme));
-  };
+  const updateTheme = useCallback(
+    (newTheme: string) => {
+      dispatch(setTheme(newTheme));
+    },
+    [dispatch]
+  );
 
-  const updateMode = (newMode: ThemeMode) => {
-    dispatch(setMode(newMode));
-  };
+  const updateMode = useCallback(
+    (newMode: ThemeMode) => {
+      dispatch(setMode(newMode));
+    },
+    [dispatch]
+  );
 
-  const updateView = (newView: ViewMode) => {
-    dispatch(setView(newView));
-  };
+  const updateView = useCallback(
+    (newView: ViewMode) => {
+      dispatch(setView(newView));
+    },
+    [dispatch]
+  );
 
-  const updateHideCompletedGames = (hide: boolean) => {
-    dispatch(setHideCompletedGames(hide));
-  };
+  const updateHideCompletedGames = useCallback(
+    (hide: boolean) => {
+      dispatch(setHideCompletedGames(hide));
+    },
+    [dispatch]
+  );
 
   return {
     theme,
