34c1303016a5b75fae0c6f8d065fb6d9e6c62cb0
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2025-12-09 10:15:47 -0500

    implement ACC and Big Ten tiebreaker configs

---
+++ lib/cfb/tiebreaker-rules/b1g/config.ts
@@ -0,0 +1,31 @@
+import { CFBConferenceTiebreakerConfig } from '../core/types';
+import { applyRuleAHeadToHead } from '../common/rule-a-head-to-head';
+import { applyRuleBCommonOpponents } from '../common/rule-b-common-opponents';
+import { applyRuleCHighestPlacedOpponent } from '../common/rule-c-highest-placed-opponent';
+import { applyRuleDOpponentWinPercentage } from '../common/rule-d-opponent-win-percentage';
+import { applyRuleETeamRatingScore } from '../common/rule-e-team-rating-score';
+
+export const CFB_B1G_TIEBREAKER_CONFIG: CFBConferenceTiebreakerConfig = {
+  rules: [
+    {
+      name: 'Head-to-Head',
+      apply: applyRuleAHeadToHead,
+    },
+    {
+      name: 'Common Opponents',
+      apply: applyRuleBCommonOpponents,
+    },
+    {
+      name: 'Highest Placed Common Opponent',
+      apply: applyRuleCHighestPlacedOpponent,
+    },
+    {
+      name: 'Opponent Win Percentage',
+      apply: applyRuleDOpponentWinPercentage,
+    },
+    {
+      name: 'Team Rating Score',
+      apply: applyRuleETeamRatingScore,
+    },
+  ],
+};
