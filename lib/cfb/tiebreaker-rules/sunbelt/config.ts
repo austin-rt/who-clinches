d63bd40e98f4935589840e0122bd7f7da044c1a2
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2025-12-09 23:19:13 -0500

    add sun belt tiebreaker configuration

---
+++ lib/cfb/tiebreaker-rules/sunbelt/config.ts
@@ -0,0 +1,37 @@
+import { CFBConferenceTiebreakerConfig } from '../core/types';
+import { applyRuleAHeadToHead } from '../common/rule-a-head-to-head';
+import { applyRuleDivisionalWinPercentage } from '../common/rule-divisional-win-percentage';
+import { applyRuleCHighestPlacedOpponent } from '../common/rule-c-highest-placed-opponent';
+import { applyRuleCommonNonDivisionalOpponents } from '../common/rule-common-non-divisional-opponents';
+import { applyRuleETeamRatingScore } from '../common/rule-e-team-rating-score';
+import { applyRuleFbsWinPercentage } from '../common/rule-fbs-win-percentage';
+
+export const CFB_SUNBELT_TIEBREAKER_CONFIG: CFBConferenceTiebreakerConfig = {
+  rules: [
+    {
+      name: 'Head-to-Head',
+      apply: applyRuleAHeadToHead,
+    },
+    {
+      name: 'Divisional Win Percentage',
+      apply: applyRuleDivisionalWinPercentage,
+    },
+    {
+      name: 'Highest Placed Common Opponent',
+      apply: applyRuleCHighestPlacedOpponent,
+    },
+    {
+      name: 'Common Non-Divisional Opponents',
+      apply: applyRuleCommonNonDivisionalOpponents,
+    },
+    {
+      name: 'Team Rating Score',
+      apply: applyRuleETeamRatingScore,
+    },
+    {
+      name: 'FBS Win Percentage',
+      apply: applyRuleFbsWinPercentage,
+    },
+  ],
+};
+
