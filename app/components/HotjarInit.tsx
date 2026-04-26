5641b4a56c214d70e80d88fd8f048425900b888e
Austin Ryan Taylor <austinrtaylor91@gmail.com>
2026-04-26 14:32:40 -0400

    gate hotjar init to prod only refs #23

--- app/components/HotjarInit.tsx
+++ app/components/HotjarInit.tsx
@@ -1,6 +1,8 @@
 import Script from 'next/script';
 
 const HotjarInit = () => {
+  if (process.env.NODE_ENV !== 'production') return null;
+
   return (
     <Script src="https://t.contentsquare.net/uxa/f80d849c7db5f.js" strategy="afterInteractive" />
   );
